ae2a1ea61fb82192407a290465b16c2438e6a12b
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-03 19:41:33 +0530

    boolean

--- Hello world/demo.py
+++ Hello world/demo.py
@@ -11,21 +11,41 @@
 
 
 #variables
-name ="Viraj"
-print(name)
+# name ="Viraj"
+# print(name)
 
-a = 3
-b = 4
-c = a + b
-print(c)
-print(a - b)
-print(a / b)
-print(a * b)
-print(a % b)
-print(a // b)#flow division - divides two numbers and rounds the result down to the nearest integer
-print(a ** b)#astric operator - a to the power b 
+# a = 3
+# b = 4
+# c = a + b
+# print(c)
+# print(a - b)
+# print(a / b)
+# print(a * b)
+# print(a % b)
+# print(a // b)#flow division - divides two numbers and rounds the result down to the nearest integer
+# print(a ** b)#astric operator - a to the power b 
 
-w1 = "hello"
-w2 = "world"
-w3 = 8
-print(w1 + w2 + str(w3))#str() - converts string to integer
+# w1 = "hello"
+# w2 = "world"
+# w3 = 8
+# print(w1 + w2 + str(w3))#str() - converts string to integer
+
+
+#boolean 
+
+
+age = 50 
+is_young = age < 18
+print(is_young)
+
+#operators can be used on boolean values
+a =  True
+b = False
+
+print(a and b)
+print(a and a)
+print(a or b)
+
+print(not a) #not - gives the opposite value of a
+
+print(a ^ b) #XOR - if 2 are equal gives false. If 2 are not equal gives true
