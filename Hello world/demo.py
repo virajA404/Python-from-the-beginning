0f24ab5b96c1925f8406738e4f612283fee10268
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-03 18:05:51 +0530

    initial commit

---
+++ Hello world/demo.py
@@ -0,0 +1,10 @@
+print("Hello, world!")
+print(123)
+print(123.5)
+print('Good evening')
+
+# escape characters 
+print("good \nnight")
+print("good \tnight")
+print("good \"night\"")
+print("good \ night")
