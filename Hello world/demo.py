4243f72200b38083b72ce33f138b52ae9f5bbbdd
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-03 18:48:38 +0530

    flow division

--- Hello world/demo.py
+++ Hello world/demo.py
@@ -1,10 +1,25 @@
-print("Hello, world!")
-print(123)
-print(123.5)
-print('Good evening')
+# print("Hello, world!")
+# print(123)
+# print(123.5)
+# print('Good evening')
 
-# escape characters 
-print("good \nnight")
-print("good \tnight")
-print("good \"night\"")
-print("good \ night")
+# # escape characters 
+# print("good \nnight")
+# print("good \tnight")
+# print("good \"night\"")
+# print("good \ night")
+
+
+#variables
+name ="Viraj"
+print(name)
+
+a = 3
+b = 4
+c = a + b
+print(c)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a % b)
+print(a // b)#flow division - divides two numbers and rounds the result down to the nearest integer
