f62b9afce63270ffca25c413bec13188afc0c65e
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-03 19:28:54 +0530

    string concatenation

--- Hello world/demo.py
+++ Hello world/demo.py
@@ -24,3 +24,8 @@ print(a * b)
 print(a % b)
 print(a // b)#flow division - divides two numbers and rounds the result down to the nearest integer
 print(a ** b)#astric operator - a to the power b 
+
+w1 = "hello"
+w2 = "world"
+w3 = 8
+print(w1 + w2 + str(w3))#str() - converts string to integer
