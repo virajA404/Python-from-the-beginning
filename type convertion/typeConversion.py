2db6faeaeb0f440a37e5056a0eb2d07d91ac29aa
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-04 09:11:29 +0530

    exercise

--- type convertion/typeConversion.py
+++ type convertion/typeConversion.py
@@ -1,21 +1,26 @@
-a = "15"
-b = 20
+# a = "15"
+# b = 20
 
-c = int(a) + b
-print(c)
+# c = int(a) + b
+# print(c)
 
-#one use of this is converting the type into the correct data type of an input
+# #one use of this is converting the type into the correct data type of an input
 
-#input()
-amount = input()
-b = 20
+# #input()
+# amount = input()
+# b = 20
 
-print(bool(amount)+b)
+# print(bool(amount)+b)
 
-print("Enter your name: ",end='') # end - have the curser on the same line
-name = input()
-print("Hello",name)
+# print("Enter your name: ",end='') # end - have the curser on the same line
+# name = input()
+# print("Hello",name)
 
-print("Enter your age: ",end='') 
-age = input()
-print("You are ",age, "years old")
+# print("Enter your age: ",end='') 
+# age = input()
+# print("You are ",age, "years old")
+
+var1 = bool("True")
+var2 = False or True
+result = var1 ^ var2
+print(result)
