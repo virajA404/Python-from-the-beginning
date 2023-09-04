100966913f4190dc11eb8fbf9c54d4e30b73c80e
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-04 08:50:30 +0530

    type conversion

---
+++ type convertion/typeConversion.py
@@ -0,0 +1,13 @@
+a = "15"
+b = 20
+
+c = int(a) + b
+print(c)
+
+#one use of this is converting the type into the correct data type of an input
+
+#input()
+amount = input()
+b = 20
+
+print(bool(amount)+b)
