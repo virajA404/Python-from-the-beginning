b83081a51543fcc7670d1243d085a01d570f8bea
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-11 12:21:39 +0530

    adding and deleting

--- Data structures/dictionary.py
+++ Data structures/dictionary.py
@@ -2,4 +2,32 @@
 x = {'1000':"Colombo", '1300':"Kaluthara"}
 x['1200'] = "Moratuwa"
 
+#value can be a list 
+x['fruits'] = ["Apple", "Mango", "Orange"] 
+
+#value can be another dictionary 
+x['books'] = {1998: "Harry Potter", 2000: "Lord of the ring" } 
+
+
+print(x)
+#to print the keys
+print(x.keys()) 
+
+#to print the values
+print(x.values()) 
+
+#accessing the dictionary
+y = x['1000'] 
+print(y) 
+
+#finding if the value is exists in the dictionary if not return 0
+z = x.get(1000, 0) 
+print(z)
+
+#delete the value from the dictionary
+del x["1000"]
+print(x)
+
+#delete the entire dictionary
+x.clear()
 print(x)
