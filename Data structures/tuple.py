810f0fd96dfd5fb17d9315b247bdab57af91ac3e
Viraj Abeywardana <abeywardana21@gmail.com>
2023-09-11 23:28:11 +0530

    tuple

--- Data structures/tuple.py
+++ Data structures/tuple.py
@@ -1,1 +1,16 @@
-#
+#Syntax
+viraj = ("viraj", 25, "Colombo", "Sri Lanka")
+
+print(type(viraj))
+print(viraj)
+
+#accessing values in tuple
+name = viraj[0]
+print(name)
+
+#count of a value in a tuple
+print(viraj.count(25))
+
+#expanding tuples - assigning values of tuple to variables
+name, age, city, country = viraj
+print(name, age, city, country)
